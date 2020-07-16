462371311df59015dd29df2bb7e0d4ee51c150dc
Harryharries <a1021604754@gmail.com>
2020-07-16 00:41:33 -0500

    add server back end

---
+++ server/index.js
@@ -0,0 +1,117 @@
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+
+app.use(require('cors')())
+app.use(express.json())
+
+app.get('/',async(req,res) =>{
+    res.send('a')
+})
+
+//add student
+app.post('/api/studemts',async(req,res)=>{
+    const student = await Student.create(req.body)
+    res.send(student)
+})
+
+//delete student
+app.post('/api/studemts/:id',async(req,res)=>{
+    await Student.findByIdAndDelete(req.params.id)
+    res.send({
+        status:true
+    })
+})
+
+//modify student
+app.get('/api/update/:id',async(req,res)=>{
+    const student = await Student.findById(req.params.id)
+    res.send(student)
+})
+// conduct update student information
+app.put('/api/confirmUpdate/:id',async(req,res)=>{
+    const student = await Student.findByIdAndUpdate(req.params.id,req.body)
+    res.send(student)
+})
+
+// query by student ID
+app.get('/api/findById/:id',async(req,res)=>{
+    const student = await Student.findById(req.params.id)
+    res.send(student)
+})
+
+// query by student name
+app.get('/api/findBynName/:name',async(req,res)=>{
+
+    var result = {
+        data:[],
+        total:''
+    }
+
+    var confident = new RegExp(req.query.name)
+
+    var query = Student.find({
+        $or:[{"name":confident}]});
+    query.count({},function(err,count){
+        if(err){
+            res.json(err)
+        }else{
+            result.total = count;
+        }
+
+    })
+    //page number
+    var pageSize = parseInt(req.query.pagzSize)
+    var currentPage = parseInt(req.query.currentPage)
+    Student.find({},(err,data)=>{
+        result.data = data
+        res.send(result)
+    }).where({$or:[{"name":confident}]}).skip((currentPage-1)*pageSize).limit(pageSize)
+})
+
+//student sublist page
+app.all('/api/studentList',async(req,res)=>{
+
+    var result = {
+        data:[],
+        total:''
+    }
+
+    var confident = new RegExp(req.query.name)
+
+    var query = Student.find({});
+    query.count({},function(err,count){
+        if(err){
+            res.json(err)
+        }else{
+            result.total = count;
+        }
+
+    })
+    //page number
+    var pageSize = parseInt(req.query.pagzSize)
+    var currentPage = parseInt(req.query.currentPage)
+    Student.find({},(err,data)=>{
+        result.data = data
+        res.send(result)
+    }).skip((currentPage-1)*pageSize).limit(pageSize)
+
+})
+
+mongoose.connect('mongodb://localhost:27017/studentbili',{useNewUrlParser:true,useFindAndModify:true,useCreateIndex:true})
+
+const Student = mongoose.model('Student',new mongoose.Schema({
+    snumber:{type:String},
+    class:{type:String},
+    name:{type:String},
+    sex:{type:String},
+    clan:{type:String},
+    javalan:{type:String},
+    dblan:{type:String}
+}))
+
+
+
+app.listen(8081,()=>{
+    console.log('success')
+})
--- src/App.vue
+++ src/App.vue
@@ -1,146 +1,175 @@
 <template>
 <el-container>
-  <el-header><i class="el-icon-edit">Student Management System</i></el-header>
-  <el-form :inline="true" :model="formInline" class="demo-form-inline" size="mini">
-  <el-form-item label="studentID">
-    <el-input v-model="formInline.user" placeholder="find"></el-input>
-  </el-form-item>
-  <el-form-item label="name">
-    <el-select v-model="formInline.region" placeholder="find2">
-      <el-option label="区域一" value="shanghai"></el-option>
-      <el-option label="区域二" value="beijing"></el-option>
-    </el-select>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit">Search</el-button>
-  </el-form-item>
-  <el-form-item>
-    <el-button type="primary" @click="onSubmit"> Create</el-button>
-  </el-form-item>
-</el-form>
-  <el-main>
-    <template>
-  <el-table
-    :data="tableData"
-    border
-    style="width: 100%">
-    <el-table-column
-      fixed
-      prop="date"
-      label="日期"
-      width="150">
-    </el-table-column>
-    <el-table-column
-      prop="name"
-      label="姓名"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="province"
-      label="省份"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="city"
-      label="市区"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      prop="address"
-      label="地址"
-      width="300">
-    </el-table-column>
-    <el-table-column
-      prop="zip"
-      label="邮编"
-      width="120">
-    </el-table-column>
-    <el-table-column
-      fixed="right"
-      label="操作"
-      width="100">
-      <template slot-scope="scope">
-        <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
-        <el-button type="text" size="small">编辑</el-button>
-      </template>
-    </el-table-column>
-  </el-table>
-</template>
-  </el-main>
+    <el-dialog title="Create New Student" :visible.sync="dialogVisible" :before-close="handleClose">
+        <el-form :inline="true" :model="addstudent" class="demo-form-inline" size="mini">
+            <el-divider content-position="left">basic information</el-divider>
+            <el-form-item label="StudentNumber">
+                <el-input v-model="addstudent.snumber" placeholder="StudentNumber" style="width:200px"></el-input>
+            </el-form-item>
+            <el-form-item label="Class">
+                <el-input v-model="addstudent.class" placeholder="Class" style="width:200px"></el-input>
+            </el-form-item>
+            <el-form-item label="Name">
+                <el-input v-model="addstudent.name" placeholder="Name" style="width:200px"></el-input>
+            </el-form-item>
+            <el-form-item label="sex" prop="resource" style="margin-left:40px ; margin-right:40px">
+                <el-radio-group v-model="addstudent.sex">
+                    <el-radio label="Male"></el-radio>
+                    <el-radio label="Female"></el-radio>
+                </el-radio-group>
+            </el-form-item>
+            <el-divider content-position="left">grades</el-divider>
+            <el-form-item label="C">
+                <el-input v-model="addstudent.clan" placeholder="StudentNumber" style="width:100px"></el-input>
+            </el-form-item>
+            <el-form-item label="JAVA">
+                <el-input v-model="addstudent.jlan" placeholder="Class" style="width:100px"></el-input>
+            </el-form-item>
+            <el-form-item label="DB">
+                <el-input v-model="addstudent.dblan" placeholder="Name" style="width:100px"></el-input>
+            </el-form-item>
+            <el-form-item>
+                <el-button type="primary" @click="onSubmit">查询</el-button>
+            </el-form-item>
+
+        </el-form>
+
+        <span slot="footer" class="dialog-footer">
+            <el-button @click="dialogVisible = false" size="mini">Cancel</el-button>
+            <el-button type="primary" @click="dialogVisible = false" size="mini">Confrim</el-button>
+        </span>
+    </el-dialog>
+
+    <el-header><i class="el-icon-edit">Student Management System</i></el-header>
+    <el-form :inline="true" :model="formInline" class="demo-form-inline" size="mini">
+        <el-form-item label="studentID">
+            <el-input v-model="formInline.user" placeholder="find"></el-input>
+        </el-form-item>
+        <el-form-item label="name">
+            <el-select v-model="formInline.region" placeholder="find2">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+            </el-select>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="primary" @click="onSubmit">Search</el-button>
+        </el-form-item>
+        <el-form-item>
+            <el-button type="success" @click="dialogVisible = true"> Create</el-button>
+        </el-form-item>
+    </el-form>
+    <el-main>
+        <template>
+            <el-table :data="tableData" border style="width: 100%">
+                <el-table-column fixed prop="date" label="日期">
+                </el-table-column>
+                <el-table-column prop="name" label="姓名">
+                </el-table-column>
+                <el-table-column prop="province" label="省份">
+                </el-table-column>
+                <el-table-column prop="city" label="市区">
+                </el-table-column>
+                <el-table-column prop="address" label="地址">
+                </el-table-column>
+                <el-table-column prop="zip" label="邮编">
+                </el-table-column>
+                <el-table-column fixed="right" label="操作">
+                    <template slot-scope="scope">
+                        <el-button @click="handleClick(scope.row)" type="primary" size="small">查看</el-button>
+                        <el-button type="danger" size="small">编辑</el-button>
+                    </template>
+                </el-table-column>
+            </el-table>
+        </template>
+    </el-main>
 </el-container>
 </template>
 
 <script>
-
-  export default {
+export default {
     data() {
-      return {
-        formInline: {
-          user: '',
-          region: ''
-        },
-        tableData: [{
-          date: '2016-05-02',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-04',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1517 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-01',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1519 弄',
-          zip: 200333
-        }, {
-          date: '2016-05-03',
-          name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1516 弄',
-          zip: 200333
-        }]
-    
-      }
+        return {
+
+            addstudent: {
+                snumber: '',
+                class: '',
+                name: '',
+                sex: '',
+                clan: '',
+                jlan: '',
+                dblan: ''
+
+            },
+            formInline: {
+                user: '',
+                region: ''
+            },
+            tableData: [{
+                date: '2016-05-02',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1518 弄',
+                zip: 200333
+            }, {
+                date: '2016-05-04',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1517 弄',
+                zip: 200333
+            }, {
+                date: '2016-05-01',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1519 弄',
+                zip: 200333
+            }, {
+                date: '2016-05-03',
+                name: '王小虎',
+                province: '上海',
+                city: '普陀区',
+                address: '上海市普陀区金沙江路 1516 弄',
+                zip: 200333
+            }],
+            dialogVisible: false
+
+        }
     },
     methods: {
-      onSubmit() {
-        console.log('submit!');
-      }
+        onSubmit() {
+            console.log('submit!');
+        }
     }
-  }
+}
 </script>
 
 <style>
-  .el-form{
-    text-align:center;
+.el-form {
+    text-align: center;
     justify-content: center;
-  }
-  body{
+}
+
+body {
     margin-top: 0px;
     margin-left: 0px;
     margin-right: 0px;
-  }
-  .el-header, .el-footer {
+}
+
+.el-header,
+.el-footer {
     background-color: #B3C0D1;
     color: #333;
     text-align: center;
     line-height: 60px;
     margin-bottom: 10px;
-  }
-  
-  .el-main {
+}
+
+/*.el-main {
     background-color: #E9EEF3;
     color: #333;
     text-align: center;
     line-height: 160px;
-  }
+  }*/
 </style>
